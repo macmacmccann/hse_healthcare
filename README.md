f7a9a99c85b5aca3356f82e16ce2ea96186e4aaa
macmacmccann <markmccann09@outlook.com>
2025-04-09 19:52:53 +0100

    ui + ux research and development

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hse_healthcare
 Healthcare Application For HSE
+
+
+Username (leave blank to use 'college'): winningteam
+Email address: x00202859@mytudublin.ie
+Password: password
+
--- health_app/login/views.py
+++ health_app/login/views.py
@@ -3,6 +3,8 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def login(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
